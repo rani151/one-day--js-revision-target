b40f9c7aad122973591beffbbdeacd6137315495
Rani  PAL <150732920+rani151@users.noreply.github.com>
2024-08-11 07:13:05 +0000

    bsics done

--- 01_basics/01_basics.js
+++ 01_basics/01_basics.js
@@ -1,1 +1,14 @@
-console.log("rani pal");
+// console.log("rani pal");
+const accountId = "12376";
+let accountEmail="rani@gmail1234";
+var accountPassword="1234";
+accountCity = "dehli";
+// console.log(accountId);
+// console.log(accountEmail);
+ let accountState ;  /*  undefined aaega kyu ki koi valune assign nhi ki */
+
+
+// CONSOLE TABLE --> CONSOLE.LOG([ ])
+console.log([accountId, accountEmail,  accountPassword, accountCity, accountState ]);
+
+/* we not prefer to use 'var'  bcz of issue in block scope and functional scope */
