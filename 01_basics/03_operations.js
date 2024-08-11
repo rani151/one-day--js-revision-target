464ecfc4600c2f62f0b6dcd9af130fbd47ff5a83
Rani  PAL <150732920+rani151@users.noreply.github.com>
2024-08-11 07:45:09 +0000

    some opresations ---in js

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,40 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+/*
+ALL BASICS OPERATIONS 
+console.log(2+2);
+console.log(2-2)
+console.log(2*2)
+console.log(2**6)
+console.log(2/2)
+console.log(2%5)
+*/
+
+let str1 = "rani";
+
+let str2 = "Pal";
+// console.log(str1 + str2);
+
+// console.log(2+2);
+// console.log("2" +2);
+// console.log(2 +"2");
+// console.log("2" + 2 + 2);
+// console.log(  2 + 2 +"5");
+
+// but this is not a good practice write-- Like
+// console.log(+true) -- 1
+// let num1, num2, num3 ;
+// num1 = num2= num3 = 2+2
+// console.log(  (2 + 2 )/ 7 - (4-7));
+
+// but this is not a good practice  in It 
+let gamescore = 100;
+// gamescore++;
+// gamescore--;
+++gamescore;
+console.log(gamescore)
+
+
+
